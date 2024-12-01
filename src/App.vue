f5533d6b49cf34b43baafef67526da10a5f13df4
Denis Kralj <denis-kralj@users.noreply.github.com>
2024-12-01 16:02:03 +0100

    Remove code not used after cleanup Add component that displays all action labels Add adding a new action Add action type Add action store using localStorage API Add uuid package for identifier generation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import FantasyRoller from './components/FantasyRoller.vue'
+import ActionCollection from './components/ActionCollection.vue'
 </script>
 
 <template>
@@ -7,39 +8,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Let's get rollin'!" />
+      <FantasyRoller msg="Let's get rollin'!" />
     </div>
   </header>
-
   <main>
+    <ActionCollection />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
