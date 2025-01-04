c90881495e85f6313acf33fc5d967eb87507a09c
the-kraljica <25987891+the-kraljica@users.noreply.github.com>
2025-01-04 18:59:09 +0100

    add SlideItem

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import ActionCollection from './components/ActionCollection.vue'
+import SlideItem from './components/SlideItem.vue';
 </script>
 
 <template>
   <main>
     <ActionCollection />
+    <SlideItem />
   </main>
 </template>
 
