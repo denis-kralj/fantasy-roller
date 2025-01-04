ea7b093489894d79e7b419335f817ac85e85defa
the-kraljica <25987891+the-kraljica@users.noreply.github.com>
2025-01-04 19:16:10 +0100

    remove components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import ActionCollection from './components/ActionCollection.vue'
-import SlideItem from './components/SlideItem.vue';
 </script>
 
 <template>
   <main>
     <ActionCollection />
-    <SlideItem />
   </main>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Quasar } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 
-// Import icon libraries
 import '@quasar/extras/material-icons/material-icons.css'
 
-// Import Quasar css
 import 'quasar/src/css/index.sass'
 
 const myApp = createApp(App)
 
 myApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
+  plugins: { Notify }
 })
 
-createApp(App).mount('#app')
+myApp.mount('#app')
