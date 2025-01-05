cdaa0379ad1ffa532323a4954998d2e10cfe8d4f
the-kraljica <25987891+the-kraljica@users.noreply.github.com>
2025-01-05 17:32:13 +0100

    remove quasar extras and notify

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { Quasar, Notify } from 'quasar'
 
-import '@quasar/extras/material-icons/material-icons.css'
-
 import 'quasar/src/css/index.sass'
 
 const myApp = createApp(App)
 
 myApp.use(Quasar, {
-  plugins: { Notify }
+  plugins: {}
 })
 
 myApp.mount('#app')
