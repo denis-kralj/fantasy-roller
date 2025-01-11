56e878787e4eee5e30741763107b33246bc19185
Denis Kralj <denis-kralj@users.noreply.github.com>
2025-01-11 22:46:18 +0100

    #20 consolidate action form use
    Used shared form for add and update action
    Added confirm dialog for delete action
    Updated action form to be used for both add and edit actions
    Added prop types to action component
    Added some basic style to make things look less crap

--- src/components/ActionComponent.vue
+++ src/components/ActionComponent.vue
@@ -1,48 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import actionStore from '@/actionStore'
+import AddActionDialog from './AddActionDialog.vue'
+import RemoveActionButton from './RemoveActionButton.vue'
+import type { Action } from '@/action'
 
-const props = defineProps(['action'])
+const props = defineProps<{ action: Action }>()
 const emit = defineEmits(['actions-updated'])
-
-const actionName = ref(props.action.Label)
-const isEditing = ref(false)
 
 function removeActionButtonClickHandler() {
     actionStore.removeAction({ Id: props.action.Id })
     emit('actions-updated')
 }
-function saveActionButtonClickHandler() {
-    const updatedAction = {
-        Label: actionName.value,
-        Id: props.action.Id,
-    }
-    actionStore.updateAction(updatedAction)
-    isEditing.value = false
-    emit('actions-updated')
-}
 </script>
 
 <template>
-    <q-item clickable v-show="!isEditing">
+    <q-item clickable>
         <q-item-section>
             <q-item-label>{{ action.Label }}</q-item-label>
         </q-item-section>
-        <q-btn @click="isEditing = true" label="Edit" color="primary" />
-        <q-btn @click="removeActionButtonClickHandler" label="Delete" color="negative" />
-    </q-item>
-    <q-item clickable v-show="isEditing">
-        <q-item-section>
-            <q-input
-                filled
-                v-model="actionName"
-                label="Action name"
-                lazy-rules
-                :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-            />
-        </q-item-section>
-        <q-btn @click="saveActionButtonClickHandler" label="Save" color="primary" />
-        <q-btn @click="isEditing = false" label="Cancel" color="negative" />
+        <AddActionDialog @actions-updated="emit('actions-updated')" :action />
+        <RemoveActionButton @action-delete-requested="removeActionButtonClickHandler" />
     </q-item>
 </template>
 
---
+++ src/components/RemoveActionButton.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['action-delete-requested'])
+const confirm = ref(false)
+</script>
+
+<template>
+    <q-btn @click="confirm = true" icon="delete" color="negative" />
+    <q-dialog v-model="confirm" persistent color="black">
+        <q-card style="background-color: black">
+            <q-card-section class="row items-center">
+                <q-avatar icon="delete" color="negative" text-color="black" />
+                <span class="q-ml-sm">Are you sure you want to delete this action?</span>
+            </q-card-section>
+
+            <q-card-actions align="right">
+                <q-btn flat label="Cancel" color="primary" v-close-popup />
+                <q-btn
+                    flat
+                    label="Delete"
+                    color="negative"
+                    v-close-popup
+                    @click="emit('action-delete-requested')"
+                />
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
+</template>
