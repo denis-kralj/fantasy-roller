726cbdc45edb8b414871963951fd3bae80e864d7
Denis Kralj <denis-kralj@users.noreply.github.com>
2025-03-16 01:03:57 +0100

    #28 Add action roll ability
    Adjust layout to make app look less yuck
    Add smooth scrolling to roll results

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <nav>
-        <RouterLink to="/"><q-btn color="primary" icon="home" /></RouterLink>
-        <RouterLink to="/manage"><q-btn color="secondary" icon="settings" /></RouterLink>
-    </nav>
-    <main>
-        <RouterView />
-    </main>
+    <q-layout view="hHh lpR fFf">
+        <q-header>
+            <nav>
+                <RouterLink to="/"><q-btn color="primary" icon="home" /></RouterLink>
+                <RouterLink to="/manage"><q-btn color="secondary" icon="settings" /></RouterLink>
+            </nav>
+        </q-header>
+        <main>
+            <RouterView />
+        </main>
+    </q-layout>
 </template>
 
 <style scoped></style>
--- src/components/ActionCollection.vue
+++ src/components/ActionCollection.vue
@@ -12,9 +12,11 @@ function actionsUpdatedEvent() {
 </script>
 
 <template>
-    <h2>Actions</h2>
-    <q-list bordered separator v-for="action in actions" :key="action.Id">
-        <ActionComponent @actions-updated="actionsUpdatedEvent" :action="action" />
-    </q-list>
-    <ActionDialog @actions-updated="actionsUpdatedEvent" />
+    <q-page-container>
+        <h2>Actions</h2>
+        <q-list bordered separator v-for="action in actions" :key="action.Id">
+            <ActionComponent @actions-updated="actionsUpdatedEvent" :action="action" />
+        </q-list>
+        <ActionDialog @actions-updated="actionsUpdatedEvent" />
+    </q-page-container>
 </template>
